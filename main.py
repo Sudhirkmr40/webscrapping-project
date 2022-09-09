fbeab4a09260dd1aa22067dd8a3bcab7b71a57bd
sudhir kumar <Sudhirkmr40@gmail.com>
2022-09-09 15:03:56 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from flask import Flask , render_template , request , jsonify
+from flask import Flask, render_template, request, jsonify
 
 from bs4 import BeautifulSoup as bs
 
 from urllib.request import urlopen as urReq
 
-flipcart_url = "https://www.flipkart.com/search?q="+"iphone11"
+flipcart_url = "https://www.flipkart.com/search?q=" + "iphone11"
 
 print(flipcart_url)
 
 print(urReq(flipcart_url))
 
-respose_website =  urReq(flipcart_url)
+respose_website = urReq(flipcart_url)
 
 print(respose_website)
 
 print(respose_website.read())
 
+data_flipcart = respose_website.read()
+
+print(bs(data_flipcart, "html.parser"))
+
+beautifyed_html = bs(data_flipcart, "html.parser")
+
+print(beautifyed_html)
+
+print(beautifyed_html.find_all("div", {"class": "_1AtVbE col-12-12"}))
